1b65d7c61665d908bf909eca1fb9a09aa87cf075
Dion Tryban <github@diontryban.com>
2023-08-16 23:09:41 -0400

    Add player movement

---
+++ entities/player/player_movement.gd
@@ -0,0 +1,15 @@
+extends CharacterBody2D
+
+@export var SPEED = 500.0
+
+
+func _physics_process(delta):
+    var direction = Input.get_axis("player_up", "player_down")
+    if direction:
+        velocity.y = direction * SPEED
+    else:
+        velocity.y = 0
+    
+    print(velocity.y)
+
+    move_and_slide()
