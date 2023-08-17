dc8c070f9239e6caf61bcfbc9943445a3e3bf241
Dion Tryban <github@diontryban.com>
2023-08-17 15:38:50 -0400

    Add enemy spawning

--- ui/ui_life.gd
+++ ui/ui_life.gd
@@ -4,7 +4,6 @@ extends Control
 func _process(delta):
     var player = Globals.get_player()
     if player:
-        $TextureRect.visible = true
         $TextureRect.size.x = 37 * player.health
     else:
         $TextureRect.size.x = 0
